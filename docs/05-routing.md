5634eb3beadcd53b487ff3cf32726fb39d840d73
Robin Vercammen <robinvercammen1993@hotmail.com>
2018-01-15 20:49:30 +0100

    routing

---
+++ docs/05-routing.md
@@ -0,0 +1,5 @@
+# Routing
+
+We willen de pokedex component laten zien wanneer we naar de root surfen. Hiervoor moeten we angualar router instellen. Dit doen we in `app.module.ts`.
+
+We moeten ook zeggen waar we willen dat de routes gerenderd worden. Dit doen we in de `app.component.ts` hier vervangen we alles door een `<route-outlet></route-outlet>`. Dit geeft aan angular aan dat een route hier gerenderd kan worden.
--- src/pokedex/src/app/app.module.ts
+++ src/pokedex/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -13,6 +14,10 @@ import { PokedexComponent } from './containers/pokedex/pokedex.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([{
+      path: '',
+      component: PokedexComponent
+    }]),
   ],
   providers: [],
   bootstrap: [AppComponent]
